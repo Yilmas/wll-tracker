d3d2bd0d6ec6462f2eaa7950eaf62292ee99101c
Nikolaj Thorup Jensen <mail@nt-jensen.dk>
2024-09-23 20:06:08 +0200

    Huh

---
+++ WLL Tracker/Logs/LogEvent.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WLL_Tracker.Logs
+{
+    public class LogEvent
+    {
+        public string Id { get; set; }
+        public string Author { get; set; }
+        public DateTime Updated { get; set; }
+        public List<string> Changes { get; set; }
+
+        public LogEvent(string id, string author, DateTime updated, List<string> changes)
+        {
+            Id = id;
+            Author = author;
+            Updated = updated;
+            Changes = changes;
+        }
+
+        public async Task SaveLog()
+        {
+            string path = "./log.txt";
+
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+                TextWriter tw = new StreamWriter(path);
+                tw.Close();
+            }
+            else if (File.Exists(path))
+            {
+                using (var sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine(JsonConvert.SerializeObject(this));
+                }
+            }
+        }
+
+    }
+}
